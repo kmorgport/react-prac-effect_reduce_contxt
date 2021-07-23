36f66e38d6e773b562a9411925e3ed105dadb15e
kmorgport <kmorgan0057@gmail.com>
2021-07-23 15:45:56 -0500

    useReduce practice

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,34 +1,77 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 
+//reducer function can be located outside of the component function
+const emailReducer = (state, action) =>{
+  if(action.type === 'USER_INPUT') {
+    return {value: action.val, isValid: action.val.includes('@')}
+  }
+  if(action.type === 'INPUT_BLUR'){
+    return { value: state.value, isValid: state.value.includes('@')}
+  }
+  return {value:'', isValid: false}
+}
+
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [emailIsValid, setEmailIsValid] = useState();
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [emailIsValid, setEmailIsValid] = useState();
   const [enteredPassword, setEnteredPassword] = useState('');
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: '',
+    isValid: false,
+  })
+
+  //useEffect is used commonly with persistence, http requests, timers etc (akin to watchers and computed props in Vue?)
+
+  //use useEffect when something needs to change when something is updated (ie compone being loaded, email updated)
+  // useEffect(()=>{
+  //   const identifier = setTimeout(()=>{
+  //     setFormIsValid(
+  //       enteredEmail.includes('@') && enteredPassword.trim().length > 6
+  //     );
+  //   }, 500)
+    
+  //   //honestly, just refer back to module 10 lesson 113 at 4:50 mark
+  //   return () => {
+  //     clearTimeout(identifier)
+  //   };
+  //   //the array watches any of these pointers included, if none of these dependencies 
+  //   //changed, useEffect will not be rerun
+
+  //   //do not need to include setters, they never change they can be omitted
+  // },[enteredEmail,enteredPassword])
+
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+    dispatchEmail({
+      type: 'USER_INPUT',
+      val: event.target.value
+    })
 
     setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
+      event.target.value.includes('@') && enteredPassword.trim().length > 6 
+    )
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
 
     setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@')
-    );
+      emailState.isValid && event.target.value.trim().length > 6
+    )
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    // setEmailIsValid(enteredEmail.includes('@'));
+    dispatchEmail({
+      type: 'INPUT_BLUR',
+
+    })
   };
 
   const validatePasswordHandler = () => {
@@ -37,7 +80,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    props.onLogin(emailState.value, enteredPassword);
   };
 
   return (
@@ -45,14 +88,14 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
+            emailState.isValid === false ? classes.invalid : ''
           }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
